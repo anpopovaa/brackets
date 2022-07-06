14c4aee82b356c0e241c13ccaa352e0effbb72b2
Anastasiya <nastyapopovaa1@gmail.com>
2022-07-06 23:35:31 +0300

    feat: add solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function brackets(str, bracketsConfig){
+  let newArr = [];
+  str = str.split('').forEach(item => {
+    let arrItem = bracketsConfig.findIndex(index => index.includes(item));
+    if(item === bracketsConfig[arrItem][1] && newArr[newArr.length - 1] === bracketsConfig[arrItem][0]) {
+      newArr.pop();
+    } else {
+      newArr.push(item);
+    }
+  })
+  return newArr.length === 0;
 }
